da6f1f460b412faa9edc642a95f467cbbcf25cc3
wadamasaya <16010418@anabuki-college.ac.jp>
2018-05-24 09:38:27 +0900

    init

---
+++ app/src/main/java/wadamasaya/hellosample/HalloSampleActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.hellosample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalloSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo_sample);
+    }
+}
