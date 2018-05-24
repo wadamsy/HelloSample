730ab5faf9f7b9ebb307a852b638626b2e3257eb
wadamasaya <16010418@anabuki-college.ac.jp>
2018-05-24 10:11:45 +0900

    test2

--- app/src/main/java/wadamasaya/hellosample/HalloSampleActivity.java
+++ app/src/main/java/wadamasaya/hellosample/HalloSampleActivity.java
@@ -2,6 +2,10 @@ package wadamasaya.hellosample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HalloSampleActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class HalloSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hallo_sample);
+
+        //表示ボタンであるButtonオブジェクトを取得。
+        Button btClick = findViewById(R.id.btClick);
+        //リスナクラスのインスタンスを生成。
+        HelloListener listener = new HelloListener();
+        //表示ボタンにリスナを設定。
+        btClick.setOnClickListener(listener);
+    }
+
+    /**
+     * ボタンをクリックしたときのリスナクラス
+     */
+    private class HelloListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            //名前入力欄であるEditTextオブジェクトを取得。
+            EditText input = findViewById(R.id.etName);
+            //メッセージを表示するTextViewオブジェクトを取得。
+            TextView output = findViewById(R.id.tvOutput);
+            //入力された名前文字列を取得。
+            String inputStr = input.getText().toString();
+            //メッセージを表示。
+            output.setText(inputStr + "さん、こんにちは！");
+        }
     }
 }
